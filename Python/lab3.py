536232f833cd202f972b9e438a782142c58244f1
DaniilSkokleenko <Daniil_Skokleenko@epam.com>
2016-09-15 13:48:30 +0300

    AISOBOI/Python/lab3

--- Python/lab3.py
+++ Python/lab3.py
@@ -1,3 +1,14 @@
+import sys
+
 def collatz(x):
-    r = []
-    
+    r = [str(x)]
+    while x != 1:
+        if x % 2 == 0:
+            x = x // 2
+        else:
+            x = 3 * x + 1
+        r.append(str(x))
+    return '->'.join(r)
+
+x = int(sys.stdin.read())
+print(collatz(x))
