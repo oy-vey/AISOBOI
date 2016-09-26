7c9aff931fbba16f89b410c811ad6d313256cd58
DaniilSkokleenko <Daniil_Skokleenko@epam.com>
2016-09-26 11:30:14 +0300

    'Tolstikov_materials'

---
+++ ModernAlgorithms/run.bat
@@ -0,0 +1,4 @@
+cache.exe 1023
+cache.exe 1024
+cache.exe 1025
+cache.exe 1055
---
+++ Python/lab4'.py
@@ -0,0 +1,7 @@
+def reverse(s):
+    a = s.split('_')
+    a.reverse()
+
+    print('_'.join(a))
+
+reverse("_____The___Day_That_Never_Comes___")
