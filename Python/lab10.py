03b08d206511a35f10aebe9b341de7e10338e1cc
DaniilSkokleenko <Daniil_Skokleenko@epam.com>
2016-10-19 12:35:26 +0300

    PEP changes

--- Python/lab10.py
+++ Python/lab10.py
@@ -1,18 +1,16 @@
 def format_print(x, level=0):
-    xtype = type(x)
+    argtype = type(x)
     brackets = {list: '[]', tuple: '()', set: '{}', dict: '{}'}
-    if xtype in [dict]:
-        return ('\t' * (level - (1 * level))) + brackets[xtype][0] + '\n' + \
+    if argtype in [dict]:
+        return ('\t' * (level - (1 * level))) + brackets[argtype][0] + '\n' + \
                     ''.join(['\t' * (level + 1) + str(i) + ':' + format_print(x[i], level + 1) + ',\n' for i in x]) + \
-               ('\t' * level) + brackets[xtype][1]
-    elif xtype in brackets.keys():
-        return ('\t' * (level - (1 * level))) + brackets[xtype][0] + '\n' + \
+               ('\t' * level) + brackets[argtype][1]
+    elif argtype in brackets.keys():
+        return ('\t' * (level - (1 * level))) + brackets[argtype][0] + '\n' + \
                     ''.join(['\t' * (level+1) + format_print(i, level + 1) + ',\n' for i in x]) + \
-               ('\t' * level) + brackets[xtype][1]
+               ('\t' * level) + brackets[argtype][1]
     else:
         return str(x)
-
-
 
 print(format_print([1, {'a': [1, 2], 'b': 'hi'}]))
 
--- Python/lab8.py
+++ Python/lab8.py
@@ -1,10 +1,6 @@
-from math import sqrt, floor
 from itertools import chain
 
-N = 4
+N = 1000
 
-primes = [i for i in chain([2], range(3, N, 2)) if all([i % d for d in range(2, floor(sqrt(i) + 1))])]
-print(primes)
-
-primes = [i for i in chain([2], range(3, N, 2)) if all([i % d for d in range(2, int(sqrt(i) + 1))])]
+primes = [i for i in chain([2], range(3, N, 2)) if all([i % d for d in range(2, int(i**0.5 + 1))])]
 print(primes)
