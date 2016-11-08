409869bf1be94b93fc7d31ba948270a67ec47d9c
DaniilSkokleenko <Daniil_Skokleenko@epam.com>
2016-11-08 14:20:06 +0300

    'Watchdog.sh'

--- UNIX/2A/watchdog.sh
+++ UNIX/2A/watchdog.sh
@@ -1,20 +1,15 @@
 #!/bin/bash
-HTML=$(wget -qO - "vk.com/$1")
-if [ -n "$HTML" ]
-then
-isHidden=$(echo $HTML | grep -o "Страница доступна только авторизованным пользователям")
-isOffline=$(echo $HTML | grep -o "заходил")
-if [ -n "$isHidden" ]
-then
-echo "Hidden information"
+user_id=$1;
+RESPONSE=$(wget -qO - "https://api.vk.com/method/users.get?user_ids=$user_id&fields=online");
+is_valid=$(echo $RESPONSE | grep -Po '(?<="error_code":)[0-9]+');
+error_msg=$(echo $RESPONSE | grep -oP '"error_msg":"\K[^"]+');
+if [ -n "${is_valid}" ]; then
+	echo "Error: "${error_msg}""; exit 1;
+fi;
+is_hidden=$(echo $RESPONSE | grep -Po '(?<="hidden":)[0-9]+');
+is_online=$(echo $RESPONSE | grep -Po '(?<="online":)[0-9]+');
+if [ -n "${is_hidden}" ]; then
+	echo "Hidden profile";
 else
-if [ -n "$isOffline" ]
-then
-echo "Offline"
-else
-echo "Online"
-fi
-fi
-else
-echo "Page does not exists"
-fi
+	case "${is_online}" in "1"*) echo "User is online" ;; "0"*) echo "User is offline" ;; esac;
+fi;
