cd7102008fe6b21c0d3a71681016b41bb2eb266d
Daniil Skokleenko <dcman1993@icloud.com>
2016-11-15 11:29:33 +0300

    bookmarks.sh

--- UNIX/2B/bookmarks.sh
+++ UNIX/2B/bookmarks.sh
@@ -8,47 +8,84 @@ show_help () {
   echo 'Options:
   -h, --help          shows help
   -s, --save <BOOKMARK_NAME> saves current directory as bookmark 
-  -g, --go <BOOKMARK_NAME> go to bookmark directory
+  -g, --go <BOOKMARK_NAME> go to bookmark directory (IMPORTANT! USE "." before this command)
   -p, --print <BOOKMARK_NAME> print bookmark directory
   -d, --delete <BOOKMARK_NAME> delete bookmark
   -l, --list list all saved bookmarks   
 '
 }
 
-changedir () {
-  newdir=$1;
-  cd $newdir;
-}
-
 function s  {
-    CURDIR=$(echo $PWD);
-    echo "export DIR_$1=\"$CURDIR\"" >> $BKMRKS;
+    # shellcheck disable=SC1091
+    source "$BKMRKS";
+    CURDIR=$($"PWD");
+    validate_bname "$1";
+    target=$(eval $(echo echo $(echo \$DIR_$1)));
+    if [ -z "$exit_message" ]; then
+      if [ -z "$target" ]; then
+        echo "export DIR_$1=\"$CURDIR\"" >> "$BKMRKS";
+      else
+        echo -e "Error: bookmark '${1}' already exists";
+      fi;
+    fi
 }
 
 function g {
-    source $BKMRKS
-    target="$(eval $(echo echo $(echo \$DIR_$1)))" #TBD
+     # shellcheck disable=SC1090
+    source "$BKMRKS";
+    target=$(eval $(echo echo $(echo \$DIR_$1)))
     if [ -d "$target" ]; then #target is an existing catalog
-        changedir $target;
-    elif [ ! -n "$target" ]; then #target is empty
-        echo -e "\033[${RED}WARNING: '${1}' bashmark does not exist\033[00m"
+        cd "$target" || exit 1;
+    elif [ -z "$target" ]; then #target is empty
+        echo -e "Error: bookmark '${1}' does not exist"
     else
-        echo -e "\033[${RED}WARNING: '${target}' does not exist\033[00m"
+        echo -e "Error: '${target}' folder does not exist anymore"
     fi
 }
 
 function p {
-    source $BKMRKS
-    echo "$(eval $(echo echo $(echo \$DIR_$1)))"
+    # shellcheck disable=SC1090
+    source "$BKMRKS"
+    target=$(eval $(echo echo $(echo \$DIR_$1)))
+    if [ -n "$target" ]; then
+      echo $(eval $(echo echo $(echo \$DIR_$1)))
+    else
+      echo -e "Error: bookmark '${1}' does not exist";
+    fi;
+    
 }
 
 function d {
-    unset "DIR_$1";
+  # shellcheck disable=SC1090
+  source "$BKMRKS"
+  validate_bname "$1";
+  target=$(eval $(echo echo $(echo \$DIR_$1)))
+  if [ -z "$exit_message" ]; then
+    if [ -n "$target" ]; then
+      unset "DIR_$1";
+      sed -i "" "/^export DIR_${1}/d" "$BKMRKS";
+    else
+      echo -e "Error: bookmark '${1}' does not exist";
+    fi;
+  fi;
+    
 }
 
 function l {
-    source $BKMRKS
+    # shellcheck disable=SC1090
+    source "$BKMRKS"
     env | sort | awk '/^DIR_.+/{split(substr($0,5),parts,"="); printf("\033[0;33m%-20s\033[0m %s\n", parts[1], parts[2]);}'
+}
+
+function validate_bname {
+    exit_message=""
+    if [ -z "$1" ]; then
+        exit_message="Error: bookmark name required"
+        echo "$exit_message"
+    elif [ "$1" != "$(echo "$1" | sed 's/[^A-Za-z0-9_]//g')" ]; then
+        exit_message="Error: Bookmark name is not valid"
+        echo "$exit_message"
+    fi
 }
 
 command=$1;
@@ -71,8 +108,10 @@ fi
 touch $BKMRKS
 
 case "${command}" 
-in "-s"|"--save"*) s $BOOKMARK_NAME ;; 
-   "-g"|"--go"*) g $BOOKMARK_NAME ;;
-   "-p"|"--print"*) p $BOOKMARK_NAME ;;
-   "-d"|"--delete"*) d $BOOKMARK_NAME ;;
-   "-l"|"--list"*) l ;;esac;
+in "-s"|"--save"*) s "$BOOKMARK_NAME" ;; 
+   "-g"|"--go"*) g "$BOOKMARK_NAME" ;;
+   "-p"|"--print"*) p "$BOOKMARK_NAME" ;;
+   "-d"|"--delete"*) d "$BOOKMARK_NAME" ;;
+   "-l"|"--list"*) l ;;
+    *) show_usage;;
+esac;
