ac13e29bd3dee7612c50fe0b323609b5896afdd1
Daniil Skokleenko <dcman1993@icloud.com>
2016-12-01 18:20:32 +0300

    Error handling added

--- UNIX/2A/watchdog.sh
+++ UNIX/2A/watchdog.sh
@@ -1,45 +1,89 @@
 #!/bin/bash
+
 #Testmacbook commit
-show_usage () {                        
+
+show_usage () {
+
   echo "usage: $(basename "$0") [-h|--help] <USERID>";
+
 }
+
+
 
 show_help () {
+
   show_usage
+
   echo 'Returns "User is online"  if vk.com/<USERID> user is online
--	"User is offline" if vk.com/<USERID> user is offline
--	"Hidden profile" if vk.com/<USERID> user is hidden
+
+- "User is offline" if vk.com/<USERID> user is offline
+
+- "Hidden profile" if vk.com/<USERID> user is hidden
+
 Options:
+
   -h, --help          shows help
+
 Exit status:
+
   1: Invalid user
+
   0: Valid user
+
 '
+
 }
 
+
+
 if [ "$#" == "0" ]; then
+
   show_usage;
+
   exit;
+
 fi
 
-if [ "$1" == "-h" ] || [ "$1" == "--help" ]; then 
+
+
+if [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
+
   show_help;
+
   exit;
+
 fi
+
+
+
+
 
 
 
 user_id=$1;
-RESPONSE=$(wget -qO - "https://api.vk.com/method/users.get?user_ids=$user_id&fields=online") || exit 1;
+
+RESPONSE=$(wget -qO - "https://api.vk.com/method/users.get?user_ids=$user_id&fields=online") || { echo  "Error. Connection problems" ; exit 1; };
+
 is_invalid=$(echo "${RESPONSE}" | grep -Po '(?<="error_code":)[0-9]+');
+
 error_msg=$(echo "${RESPONSE}" | grep -oP '"error_msg":"\K[^"]+');
+
 if [ -n "${is_invalid}" ]; then
-	(echo -e "Error: " "${error_msg}"; exit 1;)
+
+  (echo -e "Error: " "${error_msg}"; exit 1;)
+
 fi;
+
 is_hidden=$(echo "${RESPONSE}" | grep -Po '(?<="hidden":)[0-9]+');
+
 is_online=$(echo "${RESPONSE}" | grep -Po '(?<="online":)[0-9]+');
+
 if [ -n "${is_hidden}" ]; then
-	notify-send "Hidden profile";
+
+  notify-send "Hidden profile";
+
 else
-	case "${is_online}" in "1"*) notify-send "User is online" ;; "0"*) notify-send "User is offline" ;; esac;
+
+  case "${is_online}" in "1"*) notify-send "User is online" ;; "0"*) notify-send "User is offline" ;; esac;
+
 fi;
