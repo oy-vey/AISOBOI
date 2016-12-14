9eb1b1bb198ba9e828042709255d8799b62613ff
DaniilSkokleenko <Daniil_Skokleenko@epam.com>
2016-12-14 11:53:09 +0300

    Updated for reverse walkthrough

--- Python/lab16.py
+++ Python/lab16.py
@@ -1,11 +1,9 @@
 def myxrange(begin, end=None, step=1):
     (start, stop) = (0, begin) if end is None else (begin, end)
-    if begin > end:
-        (start, stop) = (stop, start)
-    while start < stop:
+    while start != stop:
         yield start
         start += step
 
 
-print list(myxrange(10, 0, -2))
-print list(xrange(10, 0, -2))
+print(list(myxrange(-1, -8, -1)))
+print(list(range(-1, -8, -1)))
