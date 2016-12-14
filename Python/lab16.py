906d83f1ece064af9cc6bf380ca71251b3e6ba7e
Daniil Skokleenko <dcman1993@icloud.com>
2016-12-14 16:33:04 +0300

    Fixed

--- Python/lab16.py
+++ Python/lab16.py
@@ -1,9 +1,27 @@
 def myxrange(begin, end=None, step=1):
     (start, stop) = (0, begin) if end is None else (begin, end)
-    while start != stop:
-        yield start
-        start += step
+    if (start < stop and step < 0) or (start > stop and step > 0):
+        raise StopIteration
+    elif start < stop:
+        while start < stop:
+            yield start
+            start += step
+    elif start > stop:
+        while start > stop:
+            yield start
+            start += step
 
 
-print(list(myxrange(-1, -8, -1)))
-print(list(range(-1, -8, -1)))
+print list(myxrange(10))
+print list(range(10))
+
+print list(myxrange(1, 10, 2))
+print list(range(1, 10, 2))
+
+print list(myxrange(10, 0, -2))
+print list(range(10, 0, -2))
+
+print list(myxrange(0, 10, -2))
+print list(range(0, 10, -2))
+
+
